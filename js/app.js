a934dbd9d83153aa25a7e562fd7cedaa314d4898
Jeff Hobson <jeffrey296@gmail.com>
2014-06-12 10:06:15 -0400

    Add preliminary Controller and begin state styling

--- js/app.js
+++ js/app.js
@@ -1,32 +1,47 @@
 /**
   * Wrapper object for game. Used to separate game from global namespace.
   */
-function SnakeGame(containerId, config) {
+function SnakeGame(containerId, userConfig) {
 
 	var defaults = {
 		GAME_WIDTH   : 30,
 		GAME_HEIGHT  : 30,
-		SCREEN_WIDTH : 750,
-		SCREEN_HEIGHT: 750
+		SCREEN_WIDTH : 720,
+		SCREEN_HEIGHT: 720,
+		SBOARD_COLOR : "#006",
+		BOARD_COLOR  : "white",
+		SNAKE_COLOR  : "black"
 	};
 
 	var constants = {
-		DIRECTION_UP     : 1,
+		DIRECTION_DOWN   : 1,
 		DIRECTION_RIGHT  : 2,
-		DIRECTION_DOWN   : -1,
+		DIRECTION_UP     : -1,
 		DIRECTION_LEFT   : -2,
-		DIRECTION_DEFAULT: 2,
+		DIRECTION_DEFAULT: 2, // Right
 		STATE_START      : 1,
 		STATE_PAUSED     : 2,
 		STATE_PLAYING    : 3,
 		STATE_END        : 4
 	};
 
-	var container = $(containerId);
-	var config = config ? merge(defaults, config) : defaultConfig;
+	var container, config, state, board, snake, scoreboard, view, controller;
+
+	container = $(containerId);
+	config = userConfig ? merge(defaults, userConfig) : defaultConfig;
 
 	this.CELL_WIDTH  = config.SCREEN_WIDTH / config.GAME_WIDTH;
 	this.CELL_HEIGHT = config.SCREEN_HEIGHT / config.GAME_HEIGHT;
+
+	this.init = function() {
+		state      = constants.STATE_START;
+		board      = new Board(config.GAME_WIDTH, config.GAME_HEIGHT);
+		snake      = new Snake();
+		view       = new View();
+		controller = new Controller();
+		view.init();
+		controller.init();
+	}
 
 	/**
 	  * Simple matrix class to be utilized by game board.
@@ -81,8 +96,6 @@ function SnakeGame(containerId, config) {
 		}
 	}
 
-	var board = new Board(config.GAME_WIDTH, config.GAME_HEIGHT);
-
 	/**
 	  * Point class for game board.
 	  */
@@ -90,15 +103,25 @@ function SnakeGame(containerId, config) {
 		this.x = x;
 		this.y = y;
 
+		// Move point in specified direction. Throw an error if
+		// no valid direction is given.
 		this.movePoint = function(heading) {
+			var newPoint = this.nextPoint(heading);
+			this.x = newPoint.x;
+			this.y = newPoint.y;
+		}
+
+		// Return the next point in the specified direction or throw
+		// an error if no valid direction is given.
+		this.nextPoint = function(heading) {
 			if (heading == constants.DIRECTION_LEFT)
-				this.x--;
+				return new Point(this.x - 1, this.y);
 			else if (heading == constants.DIRECTION_UP)
-				this.y--;
+				return new Point(this.x, this.y - 1);
 			else if (heading == constants.DIRECTION_RIGHT)
-				this.x++;
+				return new Point(this.x + 1, this.y);
 			else if (heading == constants.DIRECTION_DOWN)
-				this.y++;
+				return new Point(this.x, this.y + 1);
 			else
 				throw "Cannot calculate next point (invalid direction)";
 		}
@@ -110,16 +133,15 @@ function SnakeGame(containerId, config) {
 	function Snake(origin, direction) {
 		var head = tail = origin instanceof Point ? origin : new Point(0, 0);
 		var heading = direction || constants.DIRECTION_DEFAULT;
+		var snakeId = this.generateId();
+		console.log(snakeId);
 		this.length = 1;
 
-		// Attempt to change the heading of the snake. Returns true if
-		// the heading was changed. Return false if the direction
-		// cannot be changed (if the snake is against the wall).
+		// Attempt to change the heading of the snake. Return true if the heading
+		// was changed. Return false if the direction cannot be changed (if the snake
+		// is against the wall or an invalid direction is specified).
 		this.changeDirection = function(dir) {
-			if (!dir || dir < -2 || dir > 2 || !(dir instanceof Number))
-				throw "Invalid snake direction";
-
-			var newPoint = head.movePoint(dir);
+			var newPoint = head.nextPoint(dir);
 			if (board.isValidPoint(newPoint)) {
 				heading = dir;
 				return true;
@@ -131,8 +153,126 @@ function SnakeGame(containerId, config) {
 		// direction. The point should already be checked for validity
 		// by the game engine before calling the move() function.
 		this.move = function() {
-			head = head.movePoint(heading);
+			head.movePoint(heading);
 			this.length++;
+		}
+	}
+	/**
+	  * Persistent id is used to identify individual snakes on the game board.
+	  */
+	Snake.prototype.id = 1;
+	Snake.prototype.generateId = function() {
+		return Snake.prototype.id++;
+	}
+
+	function Scoreboard(config) {
+
+	}
+
+	/**
+	  * View class responsible for adding elements to the DOM and changing states.
+	  */
+	function View() {
+		var $parentElem = container;
+		var $board, $score, $hscore;
+
+		this.init = function() {
+			insertScoreboard();
+			insertBoard();
+			this.renderState(constants.STATE_START);
+		}
+
+		// Render specified state. Throw error if invalid state is specified.
+		this.renderState = function(state) {
+			if (state == constants.STATE_START)
+				renderStartState();
+			else if (state == constants.STATE_END)
+				renderEndState();
+			else if (state == constants.STATE_PAUSED)
+				renderPauseState();
+			else if (state == constants.STATE_PLAYING)
+				renderPlayState();
+			else
+				throw "Cannot render state: invalid state";
+		}
+
+		// Add scoreboard elements to $parentElem.
+		function insertScoreboard() {
+			var $sb = $('<div class="scoreboard" id="game-sb" />');
+			$sb.css('background-color', config.SBOARD_COLOR);
+			$score  = $('<div class="score pull-left" id="game-sb-score" />');
+			$hscore = $('<div class="score pull-right" id="game-sb-hscore" />');
+			$sb.append($score).append($hscore);
+			$sb.appendTo($parentElem);
+		}
+
+		// Add board elements to $parentElem.
+		function insertBoard() {
+			$board = $('<table class="board" id="game-board" />');
+			$board.css('background-color', config.BOARD_COLOR);
+			for (var r = 0; r < config.GAME_HEIGHT; r++) {
+				var $row = $('<tr />');
+				for (var c = 0; c < config.GAME_WIDTH; c++) {
+					$row.append('<td id="b' + r.toString() + c.toString() +'" />');
+				}
+				$row.appendTo($board);
+			}
+			$board.appendTo($parentElem);
+		}
+
+		function renderStartState() {
+			var $startOverlay = $('<div class="overlay" id="game-overlay" />');
+			var $overlayContent = $('<div class="overlay-wrapper">' +
+				'<div class="overlay-bg vertically-center">' +
+				'<div class="overlay-content text-center vertically-center">' +
+				'<p class="title">Text</p>' +
+				'<button class="">Start Game</button>' +
+				'</div></div></div>');
+			$overlayContent.appendTo($startOverlay);
+			$startOverlay.appendTo($parentElem);
+		}
+
+		function renderEndState() {
+			
+		}
+
+		function renderPauseState() {
+
+		}
+
+		function renderPlayState() {
+
+		}
+	}
+
+	/**
+	  * Controller class responsible for running the game and handling user input.
+	  */
+	function Controller() {
+		var intervalHandle;
+
+		this.init = function() {
+
+		}
+
+		this.startGame = function() {
+
+		}
+
+		this.pauseGame = function() {
+
+		}
+
+		this.resumeGame = function() {
+
+		}
+
+		this.endGame = function() {
+
+		}
+
+		this.calculateFrame = function() {
+
 		}
 	}
 
