34d14c88d82de7e629c546e314ecb8301852165d
Jeff Hobson <jeffrey296@gmail.com>
2014-06-12 12:08:43 -0400

    First working prototype

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ function SnakeGame(containerId, userConfig) {
 		SCREEN_HEIGHT: 720,
 		SBOARD_COLOR : "#006",
 		BOARD_COLOR  : "white",
-		SNAKE_COLOR  : "black"
+		SNAKE_COLOR  : "black",
+		interval     : 100
 	};
 
 	var constants = {
@@ -19,13 +20,17 @@ function SnakeGame(containerId, userConfig) {
 		DIRECTION_UP     : -1,
 		DIRECTION_LEFT   : -2,
 		DIRECTION_DEFAULT: 2, // Right
+		KEY_LEFT         : 37,
+		KEY_UP           : 38,
+		KEY_RIGHT        : 39,
+		KEY_DOWN         : 40,
 		STATE_START      : 1,
 		STATE_PAUSED     : 2,
 		STATE_PLAYING    : 3,
 		STATE_END        : 4
 	};
 
-	var container, config, state, board, snake, scoreboard, view, controller;
+	var container, config, state, board, snake, score, view, controller;
 
 	container = $(containerId);
 	config = userConfig ? merge(defaults, userConfig) : defaultConfig;
@@ -55,35 +60,53 @@ function SnakeGame(containerId, userConfig) {
 		for (var r = 0; r < this.rows; r++) {
 			matrix[r] = [];
 			for (var c = 0; c < this.cols; c++)
-				matrix[r][c] = value || null;
+				matrix[r][c] = value !== undefined ? value : null;
 		}
 
 		// Retrieve the point specified at (row, col). Throw error
-		// if (row, col) is not within bounds of the matrix.
+		// if (row, col) is not within the bounds of the matrix.
 		this.at = function(row, col) {
 			if (row < 0 || row >= this.rows ||
 				col < 0 || col >= this.cols)
 				throw "Matrix point out of bounds";
 			return matrix[row][col];
 		}
+
+		// Set cell at (row, col) to val. Throw error if (row, col)
+		// is not within the bounds of the matrix.
+		this.set = function(row, col, val) {
+			if (row < 0 || row >= this.rows ||
+				col < 0 || col >= this.cols)
+				throw "Matrix point out of bounds";
+			matrix[row][col] = val;
+		}
+
+		this.getMatrix=function(){return matrix;}
 	}
 
 	/**
 	  * Game board class.
 	  */
 	function Board(width, height) {
-		this.EMPTY_CELL = 0;
-		var grid = new Matrix(height, width, this.EMPTY_CELL);
+		var EMPTY_CELL = 0;
+		var grid = new Matrix(height, width, EMPTY_CELL);
 
 
 		// Check if point is contained within game board
 		this.isValidPoint = function(point) {
 			try {
-				grid.at(point);
+				grid.at(point.y, point.x);
 			}
 			catch (err) {
 				return false;
 			}
+			return true;
+		}
+
+		// Check if space on game board is free (equals EMPTY CELL).
+		this.isFreeSpace = function(point) {
+			if (!this.isValidPoint(point) || this.at(point) != EMPTY_CELL)
+				return false;
 			return true;
 		}
 
@@ -94,6 +117,20 @@ function SnakeGame(containerId, userConfig) {
 				throw "'point' must be instance of Point in Board.at(point)";
 			return grid.at(point.y, point.x);
 		}
+
+		// Set board element at point to specified value. Throw error if either
+		// argument is not specified, of the wrong type, or point is invalid.
+		this.set = function(point, value) {
+			if (!point || !(point instanceof Point))
+				throw "'point' must be instance of Point in Board.set(point, value)";
+			if (value === undefined)
+				throw "'value' must not be undefined in Board.set(point, value)";
+			if (!this.isValidPoint(point))
+				throw "Cannot set point: point must be valid";
+			grid.set(point.y, point.x, value);
+		}
+
+		this.getGrid=function(){return grid;}
 	}
 
 	/**
@@ -134,8 +171,11 @@ function SnakeGame(containerId, userConfig) {
 		var head = tail = origin instanceof Point ? origin : new Point(0, 0);
 		var heading = direction || constants.DIRECTION_DEFAULT;
 		var snakeId = this.generateId();
-		console.log(snakeId);
-		this.length = 1;
+		var length = 1;
+		this.color = config.SNAKE_COLOR;
+
+		if (board.isFreeSpace(head))
+			board.set(head, snakeId);
 
 		// Attempt to change the heading of the snake. Return true if the heading
 		// was changed. Return false if the direction cannot be changed (if the snake
@@ -150,11 +190,32 @@ function SnakeGame(containerId, userConfig) {
 		}
 
 		// Updates the data model with a movement in the current
-		// direction. The point should already be checked for validity
-		// by the game engine before calling the move() function.
+		// direction. If the movement puts the head in an invalid
+		// or non-empty space, return false. Otherwise, return true.
 		this.move = function() {
 			head.movePoint(heading);
-			this.length++;
+
+			// Update board and length if space is free
+			if (board.isFreeSpace(head)) {
+				board.set(head, snakeId);
+				length++;
+				return true;
+			}
+
+			// Game over if the space is not empty
+			return false;
+		}
+
+		this.getDirection = function() {
+			return heading;
+		}
+
+		this.getLength = function() {
+			return length;
+		}
+
+		this.getId = function() {
+			return snakeId;
 		}
 	}
 	/**
@@ -175,25 +236,49 @@ function SnakeGame(containerId, userConfig) {
 	function View() {
 		var $parentElem = container;
 		var $board, $score, $hscore;
+		var $overlay = $(
+			'<div class="overlay" id="game-overlay">' +
+			  '<div class="overlay-wrapper">' +
+			    '<div class="overlay-bg vertically-center">' +
+			      '<div class="overlay-content text-center vertically-center">' +
+			      '</div>' +
+			    '</div>' +
+			  '</div>' +
+			'</div>'
+		);
+		var $overlayContent;
 
 		this.init = function() {
 			insertScoreboard();
 			insertBoard();
-			this.renderState(constants.STATE_START);
+			this.renderState();
 		}
 
-		// Render specified state. Throw error if invalid state is specified.
-		this.renderState = function(state) {
+		// Render current state. Throw error if invalid state is specified.
+		this.renderState = function() {
 			if (state == constants.STATE_START)
-				renderStartState();
+				$.proxy(renderStartState, this)();
 			else if (state == constants.STATE_END)
-				renderEndState();
+				$.proxy(renderEndState, this)();
 			else if (state == constants.STATE_PAUSED)
-				renderPauseState();
+				$.proxy(renderPauseState, this)();
 			else if (state == constants.STATE_PLAYING)
-				renderPlayState();
+				$.proxy(renderPlayState, this)();
 			else
 				throw "Cannot render state: invalid state";
+		}
+
+		this.renderFrame = function() {
+			console.log('render frame');
+			console.log(state);
+			console.log(board.at(new Point(0,0)));
+			console.log(board.getGrid());
+			console.log(board.getGrid().getMatrix());
+			if (state == constants.STATE_PLAYING)
+				for (var r = 0; r < config.GAME_HEIGHT; r++)
+					for (var c = 0; c < config.GAME_WIDTH; c++)
+						if (board.at(new Point(c, r)) == snake.getId())
+							$('#' + r + 'x' + c).css('background', snake.color);
 		}
 
 		// Add scoreboard elements to $parentElem.
@@ -213,7 +298,7 @@ function SnakeGame(containerId, userConfig) {
 			for (var r = 0; r < config.GAME_HEIGHT; r++) {
 				var $row = $('<tr />');
 				for (var c = 0; c < config.GAME_WIDTH; c++) {
-					$row.append('<td id="b' + r.toString() + c.toString() +'" />');
+					$row.append('<td id="' + r + 'x' + c +'" />');
 				}
 				$row.appendTo($board);
 			}
@@ -221,19 +306,25 @@ function SnakeGame(containerId, userConfig) {
 		}
 
 		function renderStartState() {
-			var $startOverlay = $('<div class="overlay" id="game-overlay" />');
-			var $overlayContent = $('<div class="overlay-wrapper">' +
-				'<div class="overlay-bg vertically-center">' +
-				'<div class="overlay-content text-center vertically-center">' +
-				'<p class="title">Text</p>' +
-				'<button class="">Start Game</button>' +
-				'</div></div></div>');
-			$overlayContent.appendTo($startOverlay);
-			$startOverlay.appendTo($parentElem);
+			$overlayContent = $(
+				'<p class="title">begin game</p>' +
+				'<button type="button" class="btn btn-primary btn-lg"' +
+				        'id="game-start-btn">Start Game</button>'
+			);
+			
+			$overlayContent.appendTo($overlay.find('div:only-child:last'));
+			$overlay.appendTo($parentElem);
 		}
 
 		function renderEndState() {
-			
+			$overlayContent.remove();
+			$overlayContent = $(
+				'<p class="title">game over</p>' +
+				'<p>Score: '+ score + '</p>'
+			);
+
+			$overlayContent.appendTo($overlay.find('div:only-child:last'));
+			$overlay.appendTo($parentElem);
 		}
 
 		function renderPauseState() {
@@ -241,7 +332,8 @@ function SnakeGame(containerId, userConfig) {
 		}
 
 		function renderPlayState() {
-
+			$overlay.remove();
+			this.renderFrame();
 		}
 	}
 
@@ -249,14 +341,21 @@ function SnakeGame(containerId, userConfig) {
 	  * Controller class responsible for running the game and handling user input.
 	  */
 	function Controller() {
-		var intervalHandle;
+		var intervalHandle, listening, direction;
 
 		this.init = function() {
-
+			listening = false;
+			direction = snake.getDirection();
+			$('#game-start-btn').click($.proxy(this.startGame, this));
+			$('#game-resume-btn').click($.proxy(this.resumeGame, this));
 		}
 
 		this.startGame = function() {
-
+			console.log("Start game!");
+			state = constants.STATE_PLAYING;
+			view.renderState();
+			startListening();
+			intervalHandle = setInterval($.proxy(this.playFrame, this), config.interval);
 		}
 
 		this.pauseGame = function() {
@@ -268,11 +367,71 @@ function SnakeGame(containerId, userConfig) {
 		}
 
 		this.endGame = function() {
-
+			console.log("End game!");
+			clearInterval(intervalHandle);
+			stopListening();
+			state = constants.STATE_END;
+			view.renderState();
 		}
 
-		this.calculateFrame = function() {
+		this.playFrame = function() {
+			console.log('playFrame');
+			if (direction != snake.getDirection()) {
+				console.log('directions were not equal');
+				// Snake will not turn directly into wall if lined up against one.
+				// This is to prevent unnatural cases where the user might not
+				// understand how they lost since there is no visual feedback.
+				if(!snake.changeDirection(direction))
+					direction = snake.getDirection();
+			}
+			console.log(snake);
+			
+			// Game continues if snake makes a valid move
+			if (snake.move()) {
+				console.log('snake moved!');
+				score = snake.getLength();
+				view.renderFrame();
+			}
+			else {
+				console.log('game over');
+				this.endGame();
+			}
+		}
 
+		function startListening() {
+			if (!listening) {
+				$(window).keydown(keyPressed);
+				listening = true;
+			}
+		}
+
+		function stopListening() {
+			if (listening) {
+				$(window).off('keydown', keyPressed);
+				listening = false;
+			}
+		}
+
+		function keyPressed(e) {
+			switch (e.keyCode) {
+				case 27: // Esc
+					clearInterval(intervalHandle);
+					stopListening();
+					break;
+				case 37:
+					direction = constants.DIRECTION_LEFT;
+					break;
+				case 38:
+					direction = constants.DIRECTION_UP;
+					break;
+				case 39:
+					direction = constants.DIRECTION_RIGHT;
+					break;
+				case 40:
+					direction = constants.DIRECTION_DOWN;
+					break;
+			}
+			event.preventDefault();
 		}
 	}
 
